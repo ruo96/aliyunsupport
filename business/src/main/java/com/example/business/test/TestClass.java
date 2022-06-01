151d6a4875d7eb3d289acd73830073e1c348f2dd
ruo96 <ww-1096@163.com>
2022-06-01 22:47:31 +0800

    add first

---
+++ business/src/main/java/com/example/business/test/TestClass.java
@@ -0,0 +1,13 @@
+package com.example.business.test;
+
+import com.example.common.domain.AExample;
+
+/**
+ * @author wuruohong
+ * @date 2022-06-01 22:09
+ */
+public class TestClass {
+    public static void main(String[] args) {
+        AExample a = new AExample();
+    }
+}
---
+++ common/src/main/java/com/example/common/domain/AExample.java
@@ -0,0 +1,12 @@
+package com.example.common.domain;
+
+import lombok.Data;
+
+/**
+ * @author wuruohong
+ * @date 2022-06-01 18:10
+ */
+@Data
+public class AExample {
+    private String name;
+}
